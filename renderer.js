e397c54614f9ab85d177c0dc31954b6f6f970026
hendscot <hendscot@gmail.com>
2016-11-29 16:44:18 -0500

    added dynamic sidebar

--- renderer.js
+++ renderer.js
@@ -9,15 +9,25 @@ document.getElementById("close-btn").addEventListener("click", function(e) {
 });
 
 var jobj = require('./inventory.json'); //(with path)
-
 var cat = document.getElementById('cat');
+var innerCat = document.getElementById('innerCat');
 var list = jobj.Guide;
-for (var i = 0; i < list.length; i++) {
-    var listCat = list[i];
+
+populateSide();
+
+function populateSide() {
+  for (var i = 0; i < list.length; i++) {
+    var listGuide = list[i];
+    var listCat = listGuide.category;
     var li = document.createElement('li');
-    li.appendChild(document.createTextNode(listCat.title));
-    cat.appendChild(li);
-    for (var k = 0; k < listCat.length; i++) {
-        console.log(listCat.category[0].name);
+    li.appendChild(document.createTextNode(listGuide.title));
+    var ul = document.createElement('ul');
+    for (var k = 0; k < listCat.length; k++) {
+      var liIn = document.createElement('li');
+      liIn.appendChild(document.createTextNode(listCat[k].name));
+      ul.appendChild(liIn);
     }
+    li.appendChild(ul);
+    cat.appendChild(li);
+  }
 }
