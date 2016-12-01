bb48b79ef4d49c3431a577e49c4ef6ad6a330665
UPS <UPS@DICKS.local>
2016-12-01 11:51:34 -0500

    Product nav

--- renderer.js
+++ renderer.js
@@ -2,24 +2,20 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 const remote = require('electron').remote;
+const jobj   = require('./inventory.json'); //(with path)
 
 document.getElementById("close-btn").addEventListener("click", function(e) {
     var window = remote.getCurrentWindow();
     window.close();
 });
 
-var jobj = require('./inventory.json'); //(with path)
-
-var cat  = document.getElementById('cat');
-var ib   = document.getElementById('invBlock');
-var main = document.getElementById('mainWin');
-var list = jobj.Guide;
-
 populateSide();
 
 fillProduct();
 
 function populateSide() {
+  var list = jobj.Guide;
+  var cat  = document.getElementById('cat');
   for (var i = 0; i < list.length; i++) {
     var listGuide = list[i];
     var listCat = listGuide.category;
@@ -37,12 +33,19 @@ function populateSide() {
 }
 
 function fillProduct() {
+    var list = jobj.Guide;
+    var ib   = document.getElementById('invBlock');
+    var mainW = document.getElementById('mainWin');
     for (var i = 0; i < list.length; i++) {
         var listCat = list[i].category;
         for (var k = 0; k < listCat.length; k++) {
             var listProd = listCat[k].products;
             for (var j = 0; j < listProd.length; j++) {
-                
+                var prodRow = document.createElement('div');
+                prodRow.id = "invBlock";
+                prodRow.appendChild(document.createTextNode("HI"));
+                mainW.appendChild(prodRow);
+                console.log("HELLO");
             }
         }
     }
